76c9588406f60b2eb4096212a7b0753e54108403
Igor Sokolov <wardencliffe.sis@gmail.com>
2019-03-13 09:48:26 +0300

    Fix.

--- ws_api/basicPusher.go
+++ ws_api/basicPusher.go
@@ -94,22 +94,15 @@ func parseO(raw []string) []byte {
 	return nmsgO
 }
 
-func parseU(raw []string) []byte {
-	nmsgO := make([]byte, 21)
-	nmsgO[0] = 'U'                    //orderType
-	copy(nmsgO[1:4], []byte(raw[4]))  //orderID
-	copy(nmsgO[5:9], []byte(raw[4]))  //orderID
-	copy(nmsgO[9:17], []byte(raw[7])) //QTY
-	var replacer = strings.NewReplacer(" ", ",", "\t", ",")
-	str := replacer.Replace(raw[6])
-	copy(nmsgO[17:21], []byte(str)) //price int
-	return nmsgO
-}
-
 func parseX(raw []string) []byte {
-	nmsgO := make([]byte, 5)
-	nmsgO[0] = 'X'                   //orderType
-	copy(nmsgO[1:4], []byte(raw[4])) //orderID
+
+	orderID, _ := strconv.ParseUint(raw[4], 10, 32)
+
+	nmsgO := make([]byte, 7)
+	binary.BigEndian.PutUint16(nmsgO[:2], 5)                // message size
+	nmsgO[2] = 'X'                                          // type
+	binary.BigEndian.PutUint32(nmsgO[3:7], uint32(orderID)) // order ID
+
 	return nmsgO
 }
 
@@ -145,22 +138,16 @@ func main() {
 	}
 	defer fInput.Close()
 
-	//msgHeader := make([]byte, 2)
-	//var msgLength uint16
-	//var rMsgHeader *bytes.Reader
-	//msgNum := 0
-	//parseFlag := make(map[byte]bool)
+	fmt.Printf("streaming to aeron udp : %s\n", *ExamplesConfig.Channel)
+
 	// total number of all messages
 	var total uint64
-	// total number of messages for each message type
-	//totalType := make(map[byte]uint64)
 	start := time.Now()
-	fmt.Printf("=========== Parsing ITCH v5.0 starts ===========\n")
-	fmt.Printf("streaming to aeron udp : %s\n", *ExamplesConfig.Channel)
 
 	csvFile, _ := os.Open("/Users/im/github/l2_scylla_makerdao_uk/ws_api/OrderLog20181229-2.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
-	srcBuffer := atomic.MakeBuffer(([]byte)("01234567890123456789012345678901234567890123456789"))
+	srcBuffer := make([]byte, 1024)
+	srcBufferAtomic := atomic.MakeBuffer(srcBuffer, len(srcBuffer))
 	firstLine := true
 	for {
 		line, error := reader.Read()
@@ -170,15 +157,16 @@ func main() {
 			log.Fatal(error)
 		}
 		if firstLine {
+			firstLine = false
 			continue
 		}
 		for i := 0; i < len(line); i++ {
 			ord := strings.Split(line[i], ";")
 			switch ord[5] {
-			case "0": //delete = X
+			case "0": // delete = X
 				message := parseX(ord)
-				//srcBuffer := atomic.MakeBuffer(([]byte)(message))
-				ret := publication.Offer(srcBuffer, 0, int32(len(message)), nil)
+				copy(srcBuffer, message)
+				ret := publication.Offer(srcBufferAtomic, 0, int32(len(message)), nil)
 				switch ret {
 				case aeron.NotConnected:
 					log.Printf("%d: not connected yet", total)
@@ -196,8 +184,8 @@ func main() {
 				}
 			case "1": //post   = O
 				message := parseO(ord)
-
-				ret := publication.Offer(srcBuffer, 0, int32(len(message)), nil)
+				copy(srcBuffer, message)
+				ret := publication.Offer(srcBufferAtomic, 0, int32(len(message)), nil)
 				switch ret {
 				case aeron.NotConnected:
 					log.Printf("%d: not connected yet", total)
@@ -215,9 +203,8 @@ func main() {
 				}
 			case "2": //deal   = O
 				message := parseO(ord)
-
-				//srcBuffer := atomic.MakeBuffer(([]byte)(message))
-				ret := publication.Offer(srcBuffer, 0, int32(len(message)), nil)
+				copy(srcBuffer, message)
+				ret := publication.Offer(srcBufferAtomic, 0, int32(len(message)), nil)
 				switch ret {
 				case aeron.NotConnected:
 					log.Printf("%d: not connected yet", total)
@@ -236,124 +223,6 @@ func main() {
 			}
 		}
 
-		/*	for {
-				count, _ := fInput.Read(msgHeader)
-				if count < 2 {
-					// EOF
-					fmt.Printf("=========== Parsing ITCH v5.0 ends   ===========\n")
-					break
-				}
-				rMsgHeader = bytes.NewReader(msgHeader)
-				err := binary.Read(rMsgHeader, binary.BigEndian, &msgLength)
-				check(err)
-
-				// message buffer
-				// message buffer
-				message := make([]byte, msgLength+2)
-				message[0] = msgHeader[0]
-				message[1] = msgHeader[1]
-				count, _ = fInput.Read(message[2:])
-				if count < int(msgLength) {
-					panic("Error reading input file")
-				}
-				//message := fmt.Sprintf("this is a message %d", count)
-				srcBuffer := atomic.MakeBuffer(([]byte)(message))
-				log.Print(message)
-				t := message[2]
-				switch t {
-				 case 'S':
-					 log.Printf("ProcessSystemEventMessage")
-				case 'R':
-					log.Printf( "ProcessStockDirectoryMessage")
-				case 'H':
-					log.Printf( "ProcessStockTradingActionMessage")
-				case 'Y':
-					log.Printf( "ProcessRegSHOMessage")
-				case 'L':
-					log.Printf( "ProcessMarketParticipantPositionMessage")
-				case 'V':
-					log.Printf( "ProcessMWCBDeclineMessage")
-				case 'W':
-					log.Printf( "ProcessMWCBStatusMessage")
-				case 'K':
-					log.Printf( "ProcessIPOQuotingMessage")
-				case 'A':
-					log.Printf( "ProcessAddOrderMessage")
-				case 'F':
-					log.Printf( "ProcessAddOrderMPIDMessage")
-				case 'E':
-					log.Printf( "ProcessOrderExecutedMessage")
-				case 'C':
-					log.Printf( "ProcessOrderExecutedWithPriceMessage")
-				case 'X':
-					log.Printf( "ProcessOrderCancelMessage")
-				case 'D':
-					log.Printf( "ProcessOrderDeleteMessage")
-				case 'U':
-					log.Printf( "ProcessOrderReplaceMessage")
-				case 'P':
-					log.Printf( "ProcessTradeMessage")
-				case 'Q':
-					log.Printf( "ProcessCrossTradeMessage")
-				case 'B':
-					log.Printf( "ProcessBrokenTradeMessage")
-				case 'I':
-					log.Printf( "ProcessNOIIMessage")
-				case 'N':
-					log.Printf( "ProcessRPIIMessage")
-				default:
-					log.Printf( "ProcessUnknownMessage")
-
-				}
-				//log.Printf(string(t))
-				ret := publication.Offer(srcBuffer, 0, int32(len(message)), nil)
-				switch ret {
-				case aeron.NotConnected:
-					log.Printf("%d: not connected yet", total)
-				case aeron.BackPressured:
-					log.Printf("%d: back pressured", total)
-				default:
-					if ret < 0 {
-						log.Printf("%d: Unrecognized code: %d", total, ret)
-					} else {
-						log.Printf("%d: success!", total)
-					}
-				}
-				if !publication.IsConnected() {
-					log.Printf("no subscribers detected")
-				}
-				//time.Sleep(time.Second)
-			}
-		*/
-		//	fmt.Printf("Total number of all messages parsed: %d\n", total)
-		//	for t, n := range totalType {
-		//		fmt.Printf("Total number of %c messages parsed: %d\n", t, n)
-		//	}
 		fmt.Printf("Time spent: %d seconds\n", time.Since(start)/1000000000)
-
-		/**
-		for counter := 0; counter < *examples.ExamplesConfig.Messages; counter++ {
-			message := fmt.Sprintf("this is a message %d", counter)
-			srcBuffer := atomic.MakeBuffer(([]byte)(message))
-			ret := publication.Offer(srcBuffer, 0, int32(len(message)), nil)
-			switch ret {
-			case aeron.NotConnected:
-				log.Printf("%d: not connected yet", counter)
-			case aeron.BackPressured:
-				log.Printf("%d: back pressured", counter)
-			default:
-				if ret < 0 {
-					log.Printf("%d: Unrecognized code: %d", counter, ret)
-				} else {
-					log.Printf("%d: success!", counter)
-				}
-			}
-
-			if !publication.IsConnected() {
-				log.Printf("no subscribers detected")
-			}
-			time.Sleep(time.Second)
-		}
-		**/
 	}
 }
